e295e1f0600b69798a9dd62d854db5549a6b3d61
Ravi <raviyadav5951@gmail.com>
2025-11-23 22:10:24 +0400

    multiple routes

--- src/app.js
+++ src/app.js
@@ -11,6 +11,51 @@ app.get("/test", (req, res) => {
   res.send("This is a test endpoint dashboard");
 });
 
+//ab?c, ab+c, ab*c, ab(cde)?f
+//we can create regex of routes also
+
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
+
+  res.send(`User ID created`);
+});
+
+//Multiple route handlers
+
+//next can call to next route , but it should be called only if res.send is not called
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("rh1");
+    // res.send("Response from rh1");
+    next();
+  },
+
+  (req, res, next) => {
+    console.log("rh2");
+    res.send("Response from rh2");
+    next();
+  }
+);
+
+//we can also use array of handlers
+
+app.use("/usernew", [
+  (req, res, next) => {
+    console.log("rh1");
+    // res.send("Response from rh1");
+    next();
+  },
+
+  (req, res, next) => {
+    console.log("rh2");
+    res.send("Response from rh2");
+    next();
+  },
+]);
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+//advaanced routing example

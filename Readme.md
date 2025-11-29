c3ec26df2c0398da8eadb17994149cfa17f9d90f
Ravi <raviyadav5951@gmail.com>
2025-11-29 21:04:29 +0400

    schema and api level validation

--- Readme.md
+++ Readme.md
@@ -3,5 +3,20 @@
 - Error handing at the end
 
 ## Ep 6 DB schema and models
+
 - Mongoose doc [text](https://mongoosejs.com/docs/guide.html)
 - Creating schema first and then creating model
+- CRUD api methods
+- Env setup
+
+## Ep 8 DB schema validation checks
+
+- Adding validations on model : required,unique check
+- Schema types: min ,minLength
+- Custom validators by default run when inserting new document
+- To run gender validator on patch/update then we have to enable it.
+- DB schema validations on fields
+- Added timestamps on schema
+- Added schema validation for email using validator.js npm library
+- NEVER TRUST req.body (always add schema or api level validation)
+
--- src/model/user.js
+++ src/model/user.js
@@ -1,15 +1,64 @@
 //This schema will be mapped to collection in MongoDB
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const validator = require("validator");
 
-const userSchena = new Schema({
-  firstName: String,
-  lastName: String,
-  emailId: String,
-  password: String,
-  age: Number,
-  gender: String,
-});
-const User = mongoose.model("User", userSchena);
+// const userSchena = new Schema({
+//   firstName: String,
+//   lastName: String,
+//   emailId: String,
+//   password: String,
+//   age: Number,
+//   gender: String,
+// });
+
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true, minlength: 3, trim: true },
+    lastName: { type: String, trim: true },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
+      },
+    },
+    password: { type: String, trim: true },
+    age: { type: Number, min: 18 },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender must be male, female or other");
+        }
+      },
+      trim: true,
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.kindpng.com/imgv/ioJmwwJ_dummy-profile-image-jpg-hd-png-download/",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid photo url");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "Hello! I am using this application.",
+      trim: true,
+    },
+    skills: { type: [String] },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
